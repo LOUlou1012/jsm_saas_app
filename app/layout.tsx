7f16b6386ade72fc5a2e72680f3166f93f453c80
LOUlou1012 <michaellou363@gmail.com>
2026-04-27 15:53:49 +0700

    1:31:00:

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,9 @@ import type { Metadata } from "next";
 import { Bricolage_Grotesque } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
+import { ClerkProvider, Show, SignInButton, SignUpButton, UserButton } from '@clerk/nextjs'
+import { Variable } from "lucide-react";
+
 
 const bricolage = Bricolage_Grotesque({
   variable: "--font-bricolage",
@@ -21,8 +24,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${bricolage.variable} antialiased`}>
-        <Navbar/>
-        {children}
+        <ClerkProvider appearance={{ variables: { colorPrimary: '#fe5933' }}}>
+          <Navbar />
+          {children}
+        </ClerkProvider>
       </body>
     </html>
   );
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,25 +1,45 @@
+"use client" 
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 import Navitems from './Navitems'
+import {
+  UserButton,
+  SignInButton,
+  SignUpButton,
+  SignIn,
+  SignOutButton,
+  useUser
+} from "@clerk/nextjs";
+
 
 function Navbar() {
-  return (
-    <nav className='navbar'>
-        <Link href="/">
-            <div className='flex items-center gap-2.5 cursor-pointer'>
-                <Image 
-                    src="/images/logo.svg" 
-                    alt='logo' width={46} height={44}
-                />
+
+    const { isSignedIn } = useUser()
+
+    return (
+        <nav className='navbar'>
+            <Link href="/">
+                <div className='flex items-center gap-2.5 cursor-pointer'>
+                    <Image 
+                        src="/images/logo.svg" 
+                        alt='logo' width={46} height={44}
+                    />
+                </div>
+            </Link>
+            <div className='flex items-center gap-8'>
+                <Navitems/>
+
+                {!isSignedIn ? (
+            <SignInButton>
+                <button className="btn-signin">Sign In</button>
+            </SignInButton>
+            ) : (
+            <UserButton />
+            )}
             </div>
-        </Link>
-        <div className='flex items-center gap-8'>
-            <Navitems/>
-            <p>Sign In</p>
-        </div>
-    </nav>
-  )
+        </nav>
+    )
 }
 
 export default Navbar
